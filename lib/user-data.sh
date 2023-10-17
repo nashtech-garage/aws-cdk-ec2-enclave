9ee4bc0a966d360bd990fe50251d9aef6352fc24
VuiLenDi <vuilendi@gmail.com>
2023-10-17 11:05:35 +0700

    update user data

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -14,3 +14,12 @@ sudo systemctl enable --now nitro-enclaves-allocator.service
 sudo systemctl enable --now docker
 
 docker build /usr/share/nitro_enclaves/examples/hello -t hello
+
+sleep 10
+echo "Build ennclave now"
+
+build-enclave --docker-uri hello:latest --output-file hello.eif
+
+sleep 10
+echo "Run enclave"
+nitro-cli run-enclave --cpu-count 2 --memory 512 --enclave-cid 16 --eif-path hello.eif --debug-mode
